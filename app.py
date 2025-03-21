5e61071c91b73fbca0e1070a88c61156e3899f25
Yaroslav <61938366+yarov3so@users.noreply.github.com>
2025-03-20 21:01:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ st.markdown("""
 """)
 
 
-st.markdown("""*written by yarov3so*  
+st.markdown("""*crafted by yarov3so*  
 <a href="https://www.buymeacoffee.com/yarov3so" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="width: 9em; height: auto; padding-top: 1em;" ></a>""",unsafe_allow_html=True)
 
