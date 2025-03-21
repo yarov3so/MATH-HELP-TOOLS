c1c54bbd7c9d5d00c672719d527a618d6d22379d
Yaroslav <61938366+yarov3so@users.noreply.github.com>
2025-03-20 20:26:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ st.title("Math Help Tools")
 st.markdown("###### Below are tools designed to simplify tedious calculations while also serving as valuable learning resources, offering clear, step-by-step breakdowns of each process.")
 
 st.markdown("""
-- [Basic Statistical Measurements](https://basicstat.streamlit.app) 
-  Calculates range, mean, mean deviation, median and mode(s), if any
-- [Percentile Rank Calculator](https://prcalc.streamlit.app) 
+- [Basic Statistical Measurements](https://basicstat.streamlit.app)  
+  Calculates range, mean, mean deviation, median and mode(s), if any  
+- [Percentile Rank Calculator](https://prcalc.streamlit.app)  
   Calculates the percentile rank of all values in a data set  
   Supports suppressed value ranges.  
   Finds values corresponding to a given percentile rank  
 - [Stem and Lead Plot Generator](https://slplotgen.streamlit.app)  
-  Generates a compact and a full stem and leaf plot for a (reasonably well-behaved) set of values 
+  Generates a compact and a full stem and leaf plot for a (reasonably well-behaved) set of values  
 - [Double Stem and Lead Plot Generator](https://doubleslplotgen.streamlit.app)    
-  Generates a compact and a full double stem and leaf plot for two (reasonably well-behaved) sets of values
+  Generates a compact and a full double stem and leaf plot for two (reasonably well-behaved) sets of values  
 """)
 
 st.markdown("*by yarov3so*")
