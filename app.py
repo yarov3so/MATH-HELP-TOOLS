82520687e2d6cc089f04166306e4d053bb2ddadc
Yaroslav <61938366+yarov3so@users.noreply.github.com>
2025-03-20 20:47:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ st.markdown("""
 """)
 
 st.markdown("*by yarov3so*")
-st.markdown("""<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="yarov3so" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>""",unsafe_allow_html=True)
+st.markdown("""<a href="https://www.buymeacoffee.com/yarov3so" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>""",unsafe_allow_html=True)
 
