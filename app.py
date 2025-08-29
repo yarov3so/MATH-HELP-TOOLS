6b2da44179ca748488282ce1f4ca30d9f2fc3287
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-08-29 09:39:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ st.markdown("""
   Carries out the simplex method for linear optimization (minimization and maximization).  
   Thoroughly documents every step in the optimization process.
 - [Schedule Helper](https://scheduleh3lper.streamlit.app)  
-  A tool for generating and tweaking schedules primarily meant to be used by teachers in Quebec.
+  A tool for generating and tweaking schedules primarily meant to be used by teachers in Quebec.  
   Dynamically recommends a schedule respecting user-specified constraints.  
   Detects gaps, overlaps and inconsistencies.  
   Generates a Gantt chart of the recommended schedule.
